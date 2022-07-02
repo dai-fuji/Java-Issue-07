71e85f992268d2bb9306d37fdabf8ef2c4c68615
fujimoto <dai.fuji.110728@gmail.com>
2022-07-02 15:26:31 +0900

    クエリ文字列にバリデーションチェックをかける

--- src/main/java/com/fujimoto/rest/DemoController.java
+++ src/main/java/com/fujimoto/rest/DemoController.java
@@ -1,24 +1,26 @@
 package com.fujimoto.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.constraints.Pattern;
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
 
 @RestController
+@Validated
 public class DemoController {
 
     @GetMapping("/names")
-    public List<String> getName(){
-        return List.of("Ichiro","Jiro", "Saburo");
+    public String getName( @RequestParam("id") @Pattern (regexp = "^[1-3]$" ) String id) {
+        Map<Integer, String> namesById = Map.of(1,"Ichiro",2, "Jiro",3,"Saburo");
+        return namesById.get(Integer.valueOf(id));
     }
 
     @PostMapping("/names")
     public ResponseEntity<String> create(@RequestBody CreateForm form){
-        System.out.println(form);
         URI url = UriComponentsBuilder.fromUriString("http:localhost:8080")
                 .path("/names/id")
                 .build()
@@ -31,6 +33,10 @@ public class DemoController {
         return ResponseEntity.ok(Map.of("message", "name successfully updated" ));
     }
 
+    @DeleteMapping("/names/{id}")
+    public ResponseEntity<Map<String, String>> deleteForm(@PathVariable("id") int id , @RequestBody UpdateForm form){
+        return ResponseEntity.ok(Map.of("message", "name successfully deleted" ));
+    }
 
 
 
